c75c0ce1e7bf37eba2c97f13b6fce71f85a80508
Victor Araújo <victor.b_@hotmail.com>
2023-02-08 20:08:25 +0000

    Create [slug].tsx

---
+++ pages/wedding/[slug].tsx
@@ -0,0 +1,34 @@
+import { GetServerSideProps } from "next";
+import path from "path";
+import { promises as fs } from "fs";
+
+function Wedding({ groomsmen }: PlayerProps) {
+  return <div>{JSON.stringify(groomsmen)}</div>;
+}
+
+interface Params extends ParsedUrlQuery {
+  slug: string;
+}
+// This gets called on every request
+export const getServerSideProps: GetServerSideProps = async ctx => {
+  const params = ctx.params as Params;
+  //   const protocol = ctx.headers["x-forwarded-proto"] || "http";
+  //   const baseUrl = ctx ? `${protocol}://${ctx.headers.host}` : "";
+
+  const jsonDirectory = path.join(process.cwd(), "json");
+  //Read the json data file data.json
+  const fileContents = await fs.readFile(
+    jsonDirectory + "/groomsmen.json",
+    "utf8"
+  );
+  //Return the content of the data file in json format
+
+  const groomsmen = JSON.parse(fileContents).groomsmen.filter(
+    (groom: any) => groom.slug === params.slug
+  );
+
+  // Pass data to the page via props
+  return { props: { groomsmen } };
+};
+
+export default Wedding;

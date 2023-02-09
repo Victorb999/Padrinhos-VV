cfa4333feaab8b9109040ac2c10e5d9b769c9694
Victor Araújo <victor.b_@hotmail.com>
2023-02-09 00:25:50 +0000

    Update 10 files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,22 @@
-import { useEffect, useState } from "react";
-import { Groomsmen } from "../services/types";
+import { useEffect, useState } from 'react'
+import { Groomsmen } from '../services/types'
 
 export default function IndexPage() {
-  const [groomsmen, setGroomsmen] = useState<Groomsmen[]>();
+  const [groomsmen, setGroomsmen] = useState<Groomsmen[]>()
 
   const _groomsmenData = async () => {
-    const response = await fetch("/api/staticData", {
-      method: "GET"
-    });
-    return response.json();
-  };
+    const response = await fetch('/api/staticData', {
+      method: 'GET',
+    })
+    return response.json()
+  }
 
   useEffect(() => {
     // call the function
-    _groomsmenData().then(data => setGroomsmen(data.groomsmen));
+    _groomsmenData().then(data => setGroomsmen(data.groomsmen))
 
-    console.log("teste", groomsmen);
-  }, []);
+    console.log('teste', groomsmen)
+  }, [])
 
   return (
     <div className="p-4">
@@ -25,8 +25,8 @@ export default function IndexPage() {
           <div className="shadow-md flex flex-col items-center space-x-4 p-4">
             {groomsman.slug}
           </div>
-        );
+        )
       })}
     </div>
-  );
+  )
 }
--- pages/wedding/[slug].tsx
+++ pages/wedding/[slug].tsx
@@ -1,41 +1,41 @@
-import { GetServerSideProps } from "next";
-import path from "path";
-import { promises as fs } from "fs";
-import { ParsedUrlQuery } from "querystring";
+import { GetServerSideProps } from 'next'
+import path from 'path'
+import { promises as fs } from 'fs'
+import { ParsedUrlQuery } from 'querystring'
 
-import { Groomsman } from "../../services/types";
+import { Groomsman } from '../../services/types'
 
 type WeddingProps = {
-  groomsmen: Groomsman;
-};
+  groomsmen: Groomsman
+}
 
 function Wedding({ groomsmen }: WeddingProps) {
-  return <div>{JSON.stringify(groomsmen)}</div>;
+  return <div>{JSON.stringify(groomsmen)}</div>
 }
 
 interface Params extends ParsedUrlQuery {
-  slug: string;
+  slug: string
 }
 // This gets called on every request
 export const getServerSideProps: GetServerSideProps = async ctx => {
-  const params = ctx.params as Params;
-  //   const protocol = ctx.headers["x-forwarded-proto"] || "http";
+  const params = ctx.params as Params
+  //const protocol = ctx.headers["x-forwarded-proto"] || "http";
   //   const baseUrl = ctx ? `${protocol}://${ctx.headers.host}` : "";
 
-  const jsonDirectory = path.join(process.cwd(), "json");
+  const jsonDirectory = path.join(process.cwd(), 'json')
   //Read the json data file data.json
   const fileContents = await fs.readFile(
-    jsonDirectory + "/groomsmen.json",
-    "utf8"
-  );
+    jsonDirectory + '/groomsmen.json',
+    'utf8'
+  )
   //Return the content of the data file in json format
 
   const groomsmen = JSON.parse(fileContents).groomsmen.filter(
     (groom: any) => groom.slug === params.slug
-  );
+  )
 
   // Pass data to the page via props
-  return { props: { groomsmen } };
-};
+  return { props: { groomsmen } }
+}
 
-export default Wedding;
+export default Wedding

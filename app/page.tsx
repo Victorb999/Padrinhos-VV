8f93841b9bcfa96fb9ab0f7f63bd8e089b7238c0
Victor Araújo <victor.b_@hotmail.com>
2023-02-15 14:58:28 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,34 @@
 import Link from 'next/link'
-import { Groomsman } from '../services/types'
+import { Groomsman, Member } from '../services/types'
 import { getPadrinhos } from '../services/getPadrinhos'
 
 export default async function IndexPage() {
   const data = await getPadrinhos()
 
   return (
-    <div className="flex flex-wrap p-2">
+    <div className="flex flex-wrap gap-2 flex justify-center items-center">
       {data?.groomsmen.map((groomsman: Groomsman) => {
         return (
           <Link
-            className="shadow-md flexitems-center p-4 m-4 h-36 w-36 hover:bg-purple"
+            className="
+            flex flex-col justify-center items-center 
+            gap-4 p-2 m-2 
+            rounded border-4 border-purple-light shadow-lg shadow-black
+            bg-gradient-to-br	from-gray-light to-purple-light
+            w-36 h-36
+            "
             key={groomsman.id}
             href={`/${groomsman.slug}`}
           >
-            {groomsman.slug}
+            <div className="flex flex-col justify-center items-center">
+              {groomsman.members?.map((member: Member) => {
+                return (
+                  <span className="font-cursive font-bold text-dark tracking-wider text-xl">
+                    {member.nickName}
+                  </span>
+                )
+              })}
+            </div>
           </Link>
         )
       })}
